4683ec5c245a7aff76c3c35fb4c830bfa20d4574
Mijin Kim <mijin5122@naver.com>
2019-01-28 08:55:22 +0000

    D19 | 190128 PM | CR

---
+++ django/crud/posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+# def throw
+def new(request):
+    return render(request,'new.html')
+    
+# def catch
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    
+    # DB
+    post = Post(title=title,content=content)
+    post.save()
+
+    return render(request,'create.html')
+
+#
+def index(request):
+    # All Post
+    posts = Post.objects.all()
+    
+    return render(request,'index.html',{'posts':posts})

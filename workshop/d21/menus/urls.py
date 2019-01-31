17740eda63d4e4d099cebf0dbbb2306453a362f0
Mijin Kim <mijin5122@naver.com>
2019-01-31 03:26:25 +0000

    D22 | 1901 31 | static& Media

--- workshop/d21/menus/urls.py
+++ workshop/d21/menus/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from menus import views
 
+app_name = "Survey"
+
 urlpatterns = [
-    path("index/",views.index)
+    path("",views.index,name="index"),
+    path("vote/",views.vote,name="vote"),
+    path("result/",views.result,name="result"),
+    path("update/",views.update,name="update"),
 ]
--- workshop/d21/menus/views.py
+++ workshop/d21/menus/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question,Choice
 # Create your views here.
 
 def index(request):
-    quest = Question.objects.get(Question_id)
-    return render(request,"index.html",{'quest':quest})
+    quests = Question.objects.all()
+    return render(request,"index.html",{'quests':quests})
+    
+def vote(request):
+    question=Question.objects.last()
+    return render(request,"vote.html",{'question':question})
+    
+def update(request):
+    content=request.POST.get("content")
+    choice = Choice.objects.filter(content=content)
+    choice.votes+=1
+    choice.save()
+    return redirect("Survey:result")
+    
+def result(request):
+    question=Question.objects.last()
+    return render(request,"result.html",{'question':question})
